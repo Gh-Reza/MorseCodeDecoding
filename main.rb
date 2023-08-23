2a2071e4d77e14058a96583876a6a20f8bed0076
Reza Merzaie <ghrezam97@gmail.com>
2023-08-24 00:46:19 +0430

    Fix linter errors

--- main.rb
+++ main.rb
@@ -1,46 +1,48 @@
-$morse_code = {
-  ".-"    => "A",
-  "-..."  => "B",
-  "-.-."  => "C",
-  "-.."   => "D",
-  "."     => "E",
-  "..-."  => "F",
-  "--."   => "G",
-  "...."  => "H",
-  ".."    => "I",
-  ".---"  => "J",
-  "-.-"   => "K",
-  ".-.."  => "L",
-  "--"    => "M",
-  "-."    => "N",
-  "---"   => "O",
-  ".--."  => "P",
-  "--.-"  => "Q",
-  ".-."   => "R",
-  "..."   => "S",
-  "-"     => "T",
-  "..-"   => "U",
-  "...-"  => "V",
-  ".--"   => "W",
-  "-..-"  => "X",
-  "-.--"  => "Y",
-  "--.."  => "Z"
-}
+class MorseDecode
+  @morse_code = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
 
-def decode_char(ch)
-  $morse_code[ch]
+  def self.decode_char(char)
+    @morse_code[char]
+  end
+
+  def self.decode_word(word)
+    result = ''
+    word.split.each { |ch| result << decode_char(ch) }
+    result
+  end
+
+  def self.decode(str)
+    result = ''
+    str.split('  ').each { |word| result << decode_word(word) << ' ' }
+    result.strip
+  end
 end
 
-def decode_word(word)
-  result = ""
-  word.split(" ").each { |ch| result << decode_char(ch) }
-  result
-end
-
-def decode(str)
-  result = ""
-  str.split("  ").each { |word| result << decode_word(word) << " " }
-  result.strip
-end
-
-puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+puts MorseDecode.decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
